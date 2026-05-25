9333964527f0470e115a84a5af94109805076749
beyzakulac <aysebeyzakulac@gmail.com>
2026-05-25 11:13:28 +0300

    Refactor: Klasör yapısı güncellendi (Interfaces eklendi, isimlendirmeler düzeltildi)

---
+++ src/interfaces/book.js
@@ -0,0 +1,7 @@
+export const BookModel = {
+  id: null, // Benzersiz kimlik (Number - Örn: Date.now())
+  title: "", // Kitabın adı (String)
+  author: "", // Yazarın adı (String)
+  status: "Okunacak", // Okunma durumu (String - "Okunacak", "Okunuyor", "Okundu")
+  notes: "", // Kullanıcı notları (String)
+};
